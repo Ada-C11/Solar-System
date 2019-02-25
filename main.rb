c5d735e65c9410485d002804736420f11d2938bd
Mello-Cello <celloelle@gmail.com>
2019-02-25 14:48:17 -0800

    Wrote code so that the main method creates two instances of Planet and prints some of their attributes. Hard coded, for testing functionality.

--- main.rb
+++ main.rb
@@ -1,6 +1,11 @@
 require_relative "planet.rb"
 
 def main
+  jupiter = Planet.new("Jupiter", "reddish", 578944636, 75436.32, "This planet has been a planet since the big bang.")
+
+  saturn = Planet.new("Saturn", "multi-colored striped", 39435845, 222343, "This planet has rings of gas, dust and rock.")
+
+  return "#{jupiter.name} is #{jupiter.color}. #{jupiter.fun_fact} #{saturn.name} is #{saturn.color}. #{saturn.fun_fact} "
 end
 
-main
+puts main
