d6c323b15e931acef64a8b42109b2222d3688736
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-02-25 15:44:28 -0800

    Updated driver code

--- lib/main.rb
+++ lib/main.rb
@@ -1,13 +1,16 @@
 require_relative "planet"
+require_relative "solar_system"
 
 def main
+  solar_system = SolarSystem.new("Big Ol Pupper")
   waffles = Planet.new("Waffles", "\'maple syrup\' brown", 3.555e23, 1.273e12, "It is the only known " \
   "planet to be smothered in fried chicken")
   cuddles = Planet.new("Cuddles", "soft velvet grey", 9.973e30, 0.000002, "This planet loves the sun so much " \
   "that it can\'t get close enough to it!")
-
-  puts waffles.summary
-  puts cuddles.summary
+  solar_system.add_planet("Waffles")
+  solar_system.add_planet("Cuddles")
+  list = solar_system.list_planets
+  puts list
 end
 
 main
