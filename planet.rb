825352e447e56146e0499115b61188b50859c213
K <774162+kaseea@users.noreply.github.com>
2019-02-25 14:05:32 -0800

    wave 1 complete, optional exercises later

--- planet.rb
+++ planet.rb
@@ -8,17 +8,8 @@ class Planet
     @distance_from_sun_km = distance_from_sun_km
     @fun_fact = fun_fact
   end
+
+  def summary
+    return "#{self.name}: is #{self.color}, weighs #{self.mass_kg}, is #{self.distance_from_sun_km} km from the sun and #{self.fun_fact}"
+  end
 end
-
-# Load Planet into pry:
-# $ pry -r ./planet.rb
-earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "Only planet known to support life")
-
-puts earth.name
-# => Earth
-puts earth.fun_fact
-# => Only planet known to support life
-
-earth.color = "pink"
-# => NoMethodError: undefined method `color=' for #<Planet:0x00007fcfba04c130>
-# => Did you mean?  color
