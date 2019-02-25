39f3a35355dfe9a7067fe0ece6fb0f455f34f11e
Erica Norris <norrise120@gmail.com>
2019-02-25 15:18:57 -0800

    Added add_planet method to Solar System planet

---
+++ solar_system.rb
@@ -0,0 +1,25 @@
+require_relative "planet"
+
+class SolarSystem
+  attr_reader :star_name, :planets
+
+  def initialize(star_name, planets)
+    @star_name = star_name
+    @planets = []
+  end
+
+  def add_planet(planet)
+    @planets.push(planet)
+  end
+end
+
+earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "The densest planet in the solar system")
+venus = Planet.new("Venus", "red-brown", 4.867e24, 1.082e8, "Both the morning star and the evening star")
+
+sun = SolarSystem.new("sun", nil)
+
+sun.add_planet(earth)
+sun.add_planet(venus)
+
+puts sun.star_name
+puts sun.planets
