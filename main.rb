41b6d5bd5272e7c4d61ef8f0f6152b437933564b
Margaret Finnan <msfinnan@gmail.com>
2019-02-25 15:54:12 -0800

    Added add_planet and list_planets method to SolarSystem class. Updated main.rb

--- main.rb
+++ main.rb
@@ -1,11 +1,17 @@
 # main.rb
 require_relative "planet.rb"
+require_relative "solar_system.rb"
 
 def main
+  solar_system = SolarSystem.new("Sol")
   earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "Only planet known to support life")
-  puts earth.summary
   pluto = Planet.new("Pluto", "grey", 100, 500, "Is Pluto even a planet?")
-  puts pluto.summary
+
+  solar_system.add_planet(earth)
+  solar_system.add_planet(pluto)
+
+  list = solar_system.list_planets
+  puts list
 end
 
 main
--- solar_system.rb
+++ solar_system.rb
@@ -8,4 +8,30 @@ class SolarSystem
     @star_name = star_name
     @planets = []
   end
+
+  def add_planet(planet) # takes an instance of planet
+    planets << planet.name # add it to list of planets
+  end
+
+  def list_planets
+    # planets.each do |planet|
+    # puts planets
+    list_num = 1
+    # array_index = 0
+    list_string = "Planets orbiting #{star_name}\n"
+    planets.each do |planet|
+      list_string << "#{list_num}. #{planet}\n"
+      list_num += 1
+    end
+    return list_string
+  end
 end
+
+# def list_planets
+#     puts "Planets orbiting #{star_name}:"
+#     planets.each do |planet|
+#       index = 1
+#       return "#{index}. #{planet.name}"
+#       index += 1
+#     end
+#   end
