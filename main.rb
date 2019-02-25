f0e2376e6540098df60d2004750a2d16f87fa11b
Angela <angela.ohh@gmail.com>
2019-02-25 13:54:09 -0800

    fixed mars distance formatting

--- main.rb
+++ main.rb
@@ -1,6 +1,9 @@
 require_relative "planet"
 
 def main
+  earth = Planet.new("Earth", "blue-green", 5.972e24, 1.49638, "houses these things called humans")
+
+  mars = Planet.new("Mars", "scary-red", 4.930e24, 0.43523, "cool fact about mars")
 end
 
 main
