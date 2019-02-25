5c84cd7c04458c905c783c14e870c125bd1b76cd
Jessica <jessica.homet@gmail.com>
2019-02-25 14:35:39 -0800

    completed wave 2 step 4

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 
 require_relative "planet"
+require_relative "solar_system"
 
 def main
   mercury = Planet.new("Mercury", "gray", 3.285 * 10 ** 23, 57.91 * 10 ** 6, "is the smallest of the eight planets in our solar system")
@@ -7,6 +8,13 @@ def main
 
   puts mercury.summary
   puts venus.summary
+
+  solar_system = SolarSystem.new("Sol")
+  solar_system.add_planet(mercury)
+  solar_system.add_planet(venus)
+
+  list = solar_system.list_planets
+  puts list
 end
 
 main
