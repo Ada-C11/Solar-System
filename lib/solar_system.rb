13f5152199512f461169c988fc9fc34d3181c142
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-02-25 15:33:25 -0800

    Added list_planets method to SolarSystem class

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -10,7 +10,20 @@ class SolarSystem
     @planets = []
   end
 
+  # creating a method that adds a planet to the @planets array
   def add_planet(planet)
     @planets << planet
   end
+
+  # creating a method that returns a list of all planets in a given solar system
+  def list_planets
+    planet_list = "Planets orbiting #{@star_name}: "
+    @planets.each_with_index { |planet, i| planet_list += "\n#{i + 1}. #{planet}" }
+    return planet_list
+  end
 end
+
+# milky_way = SolarSystem.new("Twinkles")
+# milky_way.add_planet("Bear")
+# milky_way.add_planet("Whiskey")
+# puts milky_way.list_planets
