0f568c9633e8bdce55c318316a40a0cba1ee6556
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-02-25 14:05:57 -0800

    added attr_reader for Planets class

--- lib/planet.rb
+++ lib/planet.rb
@@ -1,9 +1,11 @@
 class Planet
-    def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
-        @name = name
-        @color = color
-        @mass_kg = mass_kg
-        @distance_from_sun_km = distance_from_sun_km
-        @fun_fact = fun_fact
-    end 
-end 
+  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+
+  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_sun_km = distance_from_sun_km
+    @fun_fact = fun_fact
+  end
+end
