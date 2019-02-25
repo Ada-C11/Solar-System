8fc6f83ee43fd4ce4c3f8d56d660672f6936bd6f
Angela <angela.ohh@gmail.com>
2019-02-25 14:22:44 -0800

    added list_planets method

--- solar_system.rb
+++ solar_system.rb
@@ -5,4 +5,15 @@ class SolarSystem
   end
 
   attr_reader :star_name, :planets
+
+  def add_planet(planet_instance)
+    @planets.push(planet_instance)
+  end
+
+  def list_planets
+    return "Planets orbiting #{@star_name}",
+           @planets.each do |planet|
+             "#{planet}"
+           end
+  end
 end
