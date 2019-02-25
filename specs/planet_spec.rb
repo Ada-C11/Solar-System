e1a5fd9188498d554195579157f9fcc58346d321
qqdipps <sdipp007@gmail.com>
2019-02-25 14:34:30 -0800

    finished writind first wave of tests.

--- specs/planet_spec.rb
+++ specs/planet_spec.rb
@@ -23,4 +23,12 @@ describe "Planet" do
   it "has a mass" do
     expect(earth.mass_kg).must_be_close_to 5.972e24, 0.01
   end
+
+  it "has a distance from sun" do
+    expect(earth.distance_from_sun_km).must_be_close_to 1.496e8, 0.01
+  end
+
+  it "has a fun fact" do
+    expect(earth.fun_fact).must_equal "Only planet known to support life"
+  end
 end
