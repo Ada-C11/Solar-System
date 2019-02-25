f0fb08a9fd1c73a3ddd9820ba7b6292bfb9068be
Stephanie Marchante <smarcha04@gmail.com>
2019-02-25 13:57:12 -0800

    added spec file for wave 1

---
+++ specs/planet_spec.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/pride'
+
+require_relative '../lib/planet'
+
+Minitest::Reporters.use!
+
+describe 'Planet Class' do
+  describe 'Constructor' do
+    it 'raise ArgumentError if mass and distance from sun are negative or non nums' do
+        expect {
+          Planet.new('Earth', 'blue-green', -1, 1.496e8, 'Only planet known to support life')
+        }.must_raise(ArgumentError)
+
+        expect {
+            Planet.new('Earth', 'blue-green', 5.972e2, -1.496e8, 'Only planet known to support life')
+        }.must_raise(ArgumentError)
+
+        expect {
+            Planet.new('Earth', 'blue-green', 'lorem', -1.496e8, 'Only planet known to support life')
+        }.must_raise(ArgumentError)
+    end
+  end
+end
