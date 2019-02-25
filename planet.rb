8c0dbeca98196fd164985aaba1a142f0b1c4289e
Angela <angela.ohh@gmail.com>
2019-02-25 14:02:19 -0800

    fixed summary output formatting

--- planet.rb
+++ planet.rb
@@ -10,21 +10,6 @@ class Planet
   attr_reader :name, :color, :mass_kg, :distance_from_the_sun_km, :fun_fact
 
   def summary
-    return "Planet name: #{@name}. \n
-    Color: #{@color} \n
-    Weight: #{@mass_kg} \n
-    Distance from sun: #{distance_from_the_sun_km} \n
-    Fun Fact: #{fun_fact}"
+    return "Planet name: #{@name}", "Color: #{@color}", "Weight: #{@mass_kg}", "Distance from sun: #{@distance_from_the_sun_km}", "Fun Fact: #{@fun_fact}"
   end
 end
-
-earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "Only planet known to support life")
-
-puts earth.name
-# => Earth
-puts earth.fun_fact
-# => Only planet known to support life
-
-earth.color = "pink"
-# => NoMethodError: undefined method `color=' for #<Planet:0x00007fcfba04c130>
-# => Did you mean?  color
