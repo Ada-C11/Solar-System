9d4f91074c1759525ff402fa2804bf47626a9bab
qqdipps <sdipp007@gmail.com>
2019-02-25 14:59:18 -0800

    added return summary statements. all tests are up to date and passing.

--- lib/planet.rb
+++ lib/planet.rb
@@ -10,6 +10,6 @@ class Planet
   end
 
   def summary
-    return ""
+    return "#{name} is a #{color} planet that is #{distance_from_sun_km} km from it's sun. It has a mass of #{mass_kg} kg and is known for #{fun_fact}."
   end
 end
