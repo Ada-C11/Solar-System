eb4f61fe4ca192d00c8296e7ff69fed0a990ac8b
Ariana Bray <aribray@gmail.com>
2019-02-25 22:31:05 -0800

    Added comments throughout

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -8,14 +8,16 @@ class SolarSystem
     @planets = []
   end
 
+  # checking to see if a planet name already exists in the solar system. If it doesn't, we'll push that instance of planet to the planets array
   def add_planet(planet)
-      planetNames = @planets.map { |p| p.name.downcase }
-      if (planetNames.include?(planet.name.downcase))
+      planet_names = @planets.map { |p| p.name.downcase }
+      if (planet_names.include?(planet.name.downcase))
         raise ArgumentError.new("This planet already exists in the solar system")
       end
       @planets << planet
   end
 
+  # creates a list of planets by iterating over the @planets array and taking the name and index
   def list_planets
     list = "Planets orbiting #{star_name}: \n"
 
@@ -25,6 +27,7 @@ class SolarSystem
     return list
   end
 
+  # uses the planet name to find information about the planet. If the input does not exist in the array, raises an argumenterror
   def find_planet_by_name(lost_planet)
       @planets.each do |planet|
         if planet.name.downcase.include?(lost_planet.downcase)
@@ -34,6 +37,7 @@ class SolarSystem
     raise ArgumentError.new("This planet does not exist in the solar system")
   end
 
+  # calculates the distance between two planets
   def distance_between(planet_1_name, planet_2_name)
     planet_1 = find_planet_by_name(planet_1_name)
     planet_2 = find_planet_by_name(planet_2_name)
@@ -41,6 +45,7 @@ class SolarSystem
     return (planet_1.distance_from_sun_km - planet_2.distance_from_sun_km).abs
   end
 
+  # creates a new planet. Any of the error messages in the methods above will apply
   def create_new_planet
     puts "Tell me a little bit about the planet you would like to add.\n What is the planet's name?"
       new_name = gets.chomp.to_s
