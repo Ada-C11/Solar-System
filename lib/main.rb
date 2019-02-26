700c16c8537eb2a7392e08a3bfe15ddc1973a88a
Stephanie Marchante <smarcha04@gmail.com>
2019-02-26 13:33:21 -0800

    added user reprompt and finished menu

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,7 @@
 # main.rb
 require_relative 'planet'
 require_relative 'solar_system'
-
+require 'colorize'
 
 def main_menu
   puts "Welcome to the Elder Scrolls Planet Program!"
@@ -9,13 +9,11 @@ def main_menu
     |1|::List Planets
     |2|::Planet Details
     |3|::Add Planet
-    |4|::Distance Between Two Planets
-    |5|::Exit"
+    |4|::Exit"
   main
 end
 
 def main
-  # ...do stuff with planets...
   morrowind = Planet.new('Morrowind', 'green', 5.972e24, 1.496e8, 'The Dunmer of Morrowind are immune to all kinds of fire')
   skyrim = Planet.new('Skyrim', 'blue', 6.4171e23, 2.279e8, 'Skyrim Nords believe in the tale of the Dragonborn')
   elsweyr = Planet.new('Elsweyr', 'orange', 6.4171e23, 2.279e8, 'Khajit are the stealthy feline inhabitants of Elsweyr')
@@ -28,10 +26,10 @@ def main
   user_input = "y"
   while user_input == "y"
     puts $\
-    puts "Make a selection:"
+    puts "Choose from the menu:"
     choice = gets.chomp.to_i
     while choice != 1 && choice != 2 && choice != 3 && choice != 4 && choice != 5
-      puts "Sorry milk-drinker, that's not a valid input. Try again."
+      puts "Sorry milk-drinker, that's not a valid input. Try again.".colorize(:red)
       choice = gets.chomp.to_i
     end 
   
@@ -43,51 +41,41 @@ def main
 
     elsif choice == 2
       puts $\
-      puts "||::Let's find out more about the planets of Tamriel!:::::||"
-      puts "||::Enter the name of the planet you want to learn about::||"
+      puts "||::Let's find out more about the planets of Tamriel!:::::||".colorize(:magenta)
+      puts "||::Enter the name of the planet you want to learn about::||".colorize(:magenta)
       puts $\
       puts list
-      discover_input = gets.chomp
-      found_planet = solar_system.find_planet_by_name(discover_input)
-      
-      if found_planet
-        puts "I had you figured for a scholar. I'll tell you all about #{found_planet.name}!"
-        puts $\
-        puts found_planet.summary
+      discover_input = gets.chomp 
+      searched_planet = solar_system.find_planet_by_name(discover_input)
+
+      if discover_input != @planets
+        puts "Sorry milk-drinker, that's not a valid input. Try again.".colorize(:red)
+        discover_input = gets.chomp
+        searched_planet = solar_system.find_planet_by_name(discover_input)
       end
-    
+
+      if searched_planet
+        puts "I had you figured for a scholar. I'll tell you all about #{searched_planet.name}!".colorize(:cyan)
+        puts $\
+        puts searched_planet.summary
+      end 
+
     elsif choice == 3
-      puts $\ 
-      puts "To create your own planet, let's input some details below:"
-
-      print "Name: "
-      user_name = gets.chomp.capitalize
-      print "Color: "
-      user_color = gets.chomp
-      print "Mass kg: "
-      user_mass = gets.chomp
-      print "Distance km: "
-      user_distance = gets.chomp
-      print "Fun fact: "
-      user_fact = gets.chomp
-
-      new_planet = Planet.new(user_name, user_color, user_mass, user_distance, user_fact)
-      solar_system.add_planet(new_planet)
-    end
+      solar_system.make_planet
+    elsif choice == 4
+      answer = "y"
+      while answer == "y"
+         puts "Would you like to run again? y || n".colorize(:yellow)
+         answer = gets.chomp
+             if answer == "y"
+                 main_menu
+             else 
+                 break
+             end
+      end
+      exit
+    end   
   end
-
-
-  #puts earth.summary
-  #puts mars.summary
-
-  # list = solar_system.list_planets
-  # puts list
-
-  # puts $/ 
-
-  # found_planet = solar_system.find_planet_by_name('Morrowind')
-  # puts found_planet.name
-
 end
 
 main_menu
--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -11,22 +11,43 @@ class SolarSystem
   def add_planet(planet)
     @planets << planet
   end
+
   # method for printing list of planets
   def list_planets
     list = ""
     @planets.length.times do |index|
       list = list + "#{index + 1}.#{planets[index].name} \n"
     end
-    return "Planets orbiting #{@star_name}:\n #{list}"
+    return "Planets orbiting #{@star_name}:\n#{list}"
   end
+
   # method for seaching planets
   def find_planet_by_name(planet_name)
     return @planets.find {|planet| planet.name.upcase == planet_name.upcase}
   end
 
-  def distance_between(planet1="", planet2="")
-    planet1 = find_planet_by_name(name1)
-    planet2 = find_planet_by_name(name2)
-    return (distance_betweenplanet1.distance_from_sun_km - planet2.distance_from_sun_km)
+#   def distance_between(planet1="", planet2="")
+#     planet1 = find_planet_by_name(name1)
+#     planet2 = find_planet_by_name(name2)
+#     return (distance_betweenplanet1.distance_from_sun_km - planet2.distance_from_sun_km)
+#   end
+
+  def make_planet
+    puts $\ 
+    puts "To create your own planet, let's input some details below:"
+
+    puts "Name: "
+    user_name = gets.chomp.capitalize
+    puts "Color: "
+    user_color = gets.chomp
+    puts "Mass in kg: "
+    user_mass = gets.chomp.to_f
+    puts "Distance in km: "
+    user_distance = gets.chomp.to_f
+    puts "Super fun fact: "
+    user_fact = gets.chomp
+
+    new_planet = Planet.new(user_name, user_color, user_mass, user_distance, user_fact)
+    add_planet(new_planet)
   end
 end
