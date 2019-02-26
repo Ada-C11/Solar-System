1967a1d0291992b93100888204c09134d29fb90a
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-02-25 21:26:38 -0800

    added find_planet_by_name and refactored list_planets method

--- lib/main.rb
+++ lib/main.rb
@@ -13,11 +13,15 @@ def main
   solar_system.add_planet(saturn)
 
   list = solar_system.list_planets
+  puts list
 
-  puts "Planets orbiting #{solar_system.star_name}:"
-  list.each_with_index do |planet, i|
-    puts "#{i + 1}. #{planet}"
-  end
+  found_planet = solar_system.find_planet_by_name("Earth")
+
+  # found_planet is an instance of class Planet
+  puts found_planet
+  # => #<Planet:0x00007fe7c2868ee8>
+
+  puts found_planet.summary
 end
 
 main
--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -7,14 +7,26 @@ class SolarSystem
   end
 
   def add_planet(planet)
-    @planets << planet.name
+    @planets << planet
   end
 
   def list_planets
-    @planets.each do |planet|
-      next
+    list = "Planets orbiting #{star_name}"
+
+    @planets.each_index do |i|
+      list += "\n#{i + 1}. #{@planets[i].name}"
     end
-    return @planets
+
+    return list
+  end
+
+  def find_planet_by_name(name)
+    @planets.each do |planet|
+      if planet.name.downcase == name.downcase
+        return planet
+      end
+      return nil
+    end
   end
 end
 
