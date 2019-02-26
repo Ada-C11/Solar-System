7a146db5b7c7766fb1588b86a1371396e8ae940e
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-02-25 16:25:47 -0800

    added planets to solar system and printed list of planets

--- lib/main.rb
+++ lib/main.rb
@@ -1,11 +1,23 @@
 require_relative "planet"
+require_relative "solar_system"
 
 def main
   earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "Only planet known to support life")
-  puts earth.summary
+  #   puts earth.summary
 
   saturn = Planet.new("Saturn", "brownish", 6.01e24, 2.4e8, "some fact")
-  puts saturn.summary
+  #   puts saturn.summary
+
+  solar_system = SolarSystem.new("Sol")
+  solar_system.add_planet(earth)
+  solar_system.add_planet(saturn)
+
+  list = solar_system.list_planets
+
+  puts "Planets orbiting #{solar_system.star_name}:"
+  list.each_with_index do |planet, i|
+    puts "#{i + 1}. #{planet}"
+  end
 end
 
 main
--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -7,9 +7,18 @@ class SolarSystem
   end
 
   def add_planet(planet)
-    @planets << planet 
+    @planets << planet.name
+  end
+
+  def list_planets
+    @planets.each do |planet|
+      next
+    end
+    return @planets
   end
 end
 
-milky_way = SolarSystem.new("milky_way")
-milky_way.add_planet
+# milky_way = SolarSystem.new("milky_way")
+# milky_way.add_planet("Earth")
+
+# puts milky_way.planets
