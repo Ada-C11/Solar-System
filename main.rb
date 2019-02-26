f7b7cd48b1849dca1827edbb65151bd052561001
Pauline Sauget <paulinevsauget@gmail.com>
2019-02-25 17:27:15 -0800

    Added methods to add & list planets, lookup by name & calculate distance

--- main.rb
+++ main.rb
@@ -1,18 +1,33 @@
-# main.rb
 require_relative 'planet'
+require_relative 'solar_system'
 
 def main
+  solar_system = SolarSystem.new('Sol')
 
-mercury = Planet.new('Mercury', 'dark-gray', 3.285e23, 57.91e8, 'First rock from the sun')
+  mercury = Planet.new('Mercury', 'dark-gray', 3.285e23, 57.91e8, 'First rock from the sun')
+  solar_system.add_planet(mercury)
+  venus = Planet.new('Venus', 'reddish-brown', 4.867e24, 67.24e8, 'Something something about Venus')
+  solar_system.add_planet(venus)
+  earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+  solar_system.add_planet(earth)
 
-earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-  
+  puts "---Testing output---"
+  puts earth.name
+  puts earth.fun_fact 
+  puts earth.summary
 
-puts earth.name
-puts earth.fun_fact 
+  puts "---Testing add & list planet---"
+  list = solar_system.list_planets
+  puts list
 
-puts mercury.summary
+  puts "---Testing find planet by name---"
+  found_planet = solar_system.find_planet_by_name('Earth')
+  puts "Found Planet: #{found_planet}"
+  puts "Planet Summary: \n#{found_planet.summary}"
 
+  puts "---Testing distance between planets---"
+  distance = solar_system.distance_between('Earth', 'Mercury')
+  puts "Distance: #{distance}"
 end
 
 main
---
+++ solar_system.rb
@@ -0,0 +1,41 @@
+require_relative 'planet'
+
+class SolarSystem
+  attr_reader = :star_name, :planets
+
+  def initialize(star_name)
+    @star_name = star_name
+    @planets = []
+  end
+
+  def add_planet(planet)
+    @planets.push(planet)
+  end
+
+  def list_planets
+    list_of_planets = "Planets orbiting #{@star_name} \n"
+    @planets.each_with_index do |planet, index|
+      # Concatenate string each loop
+      list_of_planets += "#{index+1}. #{planet.name} \n"
+    end
+    return list_of_planets
+  end
+
+  def find_planet_by_name(planet_name)
+    matching_planets = @planets.select{|planet| planet.name.downcase == planet_name.downcase}
+    if matching_planets.length == 1
+      return matching_planets[0]
+    elsif matching_planets.length > 1
+      return matching_planets
+    else
+      puts "There is no such planet!"
+    end
+  end
+
+  def distance_between(planet_one, planet_two)
+    dist_one = find_planet_by_name(planet_one).distance_from_sun_km
+    dist_two = find_planet_by_name(planet_two).distance_from_sun_km
+    distance = (dist_one - dist_two).abs
+    return distance
+  end
+end
