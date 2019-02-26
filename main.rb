467a148cdd91098ff8ce0f60c77c7620dc9d09ac
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-02-25 16:20:11 -0800

    added functionality with new relative file: solar_system.rb

--- main.rb
+++ main.rb
@@ -1,10 +1,21 @@
 require_relative "./planet.rb"
+require_relative "./solar_system.rb"
 
 def main
-  cheese_moon = Planet.new("Cheese Moon", "orange", 500, 12500, "Made of a delicious brie-gruyere blend")
-  fire_ball = Planet.new("Big Ball of Fire", "red", 100, 42000, "Gives the worst sunburn of all planets")
-  puts cheese_moon.summary
-  puts fire_ball.summary
+  cheese = Planet.new("Cheese", "orange", 500, 12500, "Made of a delicious brie-gruyere blend")
+  fire_ball = Planet.new("Fireball", "red", 100, 42000, "Gives the worst sunburn of all planets")
 end
 
-main
+solar_system = SolarSystem.new("Pretty Big Star")
+
+cheese_moon = Planet.new("Cheese", "orange", 500, 12500, "Made of a delicious brie-gruyere blend")
+solar_system.add_planet(cheese_moon)
+
+fire_ball = Planet.new("Fireball", "red", 100, 42000, "Gives the worst sunburn of all planets")
+solar_system.add_planet(fire_ball)
+
+list = solar_system.list_planets
+puts list
+
+found_planet = solar_system.find_planet_by_name("Fireball")
+puts found_planet.name
