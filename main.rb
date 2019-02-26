3e140b6178ca308cef39bc71236aa1e7ead1ca7b
Tatiana Quintana <tatsqui@gmail.com>
2019-02-25 17:24:18 -0800

    "changing main to control loop"

--- main.rb
+++ main.rb
@@ -2,17 +2,13 @@ require_relative 'planet'
 require_relative 'solar_system'
 
 def main
-  #... do stuff with planets...
+  #... making default planets in list...
   venus = Planet.new(name: "Venus", color: 'purple', mass_kg: '3.88e10', distance_from_sun_km: '1.3e8', fun_fact: 'this planet is commonly associated with feminine energy.')
 
   earth = Planet.new(name: "Earth", color: 'blue-green', mass_kg: '5.972e24', distance_from_sun_km: '1.496e8', fun_fact: 'Only planet known to support life')
 
   pluto = Planet.new(name: "Pluto", color: 'icy-grey', mass_kg: '0.887e10', distance_from_sun_km: '15.3999e8', fun_fact: 'Pluto was recently debunked from the official planet list!')
 
-  # pluto_2 = Planet.new(name: "Pluto", color: 'icy-grey', mass_kg: '0.887e10', distance_from_sun_km: '15.3999e8', fun_fact: 'Pluto recently was debunked from the official planet list!')
-
-  puts venus.summary 
-  puts pluto.summary
 
   #instantiate my new solar system named sol
   sol_system = SolarSystem.new('Sol') 
@@ -20,14 +16,30 @@ def main
   # adding planets to sol_sytem with add_planet method in solar_system.rb
   sol_system.add_planet(venus) 
   sol_system.add_planet(pluto)
-  # sol_system.add_planet(pluto_2)
+  sol_system.add_planet(earth)
 
   list = sol_system.list_planets
+
+  learn = true
+
+  while true
+  puts "Hello and welcome to the \'#{sol_system.star_name}\' solar system."
+
   puts list
 
-  # find_planet = sol_system.find_planet_by_name('pluto')
+  puts "If you would like to see a list of planets enter \'list\' or \'l\'."
+
+  puts "If you would like to learn more about a planet"
   
-  # find_planet.each { |name| puts name.summary }
+  puts list
+
+  puts "What planet would you like to learn more about?"
+  
+  end
 end
 
 main
+
+# user_input = gets.chomp
+  # find_planet = sol_system.find_planet_by_name('pluto')
+  # find_planet.each { |name| puts name.summary }
