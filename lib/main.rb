4dc77beae0d09f030d20fa0963d193bcce857ec5
qqdipps <sdipp007@gmail.com>
2019-02-25 16:56:07 -0800

    added solar_system model to main

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,15 @@
 require_relative "planet.rb"
 require_relative "solar_system.rb"
 
-def main.rb
+def main
+  solar_system = SolarSystem.new("Sol")
+
+  earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "Only planet known to support life")
+  solar_system.add_planet(Planet.new("Willer", "blue-green", 5.972e24, 1.496e8, "Only planet known to support life"))
+  solar_system.add_planet(Planet.new("Saver", "amber", 9.34e54, 2.345e23, "first planet in the universe"))
+  solar_system.add_planet(Planet.new("Kater", "green", 2.342e33, 4.234e43, "only planet to be made of dolphins"))
+  solar_system.add_planet(Planet.new("Singher", "onyx", 4.23e23, 46.2e23, "only planet to emmit song-like sounds"))
+  puts solar_system.list_planets
 end
 
 main
