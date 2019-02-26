2d129b4ec1e51452aacd951883a630737af544b2
Grace Shea <grace.m.shea@gmail.com>
2019-02-26 08:32:24 -0800

    get_info_new_planet method written...this should help.

--- solar_system.rb
+++ solar_system.rb
@@ -14,6 +14,28 @@ class SolarSystem
     @planets << planet
   end
 
-  #create list planets method
+  def get_info_new_planet
+    print "\nEnter new planet name: "
+    planet_name = gets.chomp.to_s
+    print "\nDescription of #{planet_name}'s color: "
+    color = gets.chomp.to_s
+    print "\nDistance of planet from #{@star_name}(km): "
+    distance = gets.chomp.to_f
+    print "\nMass of #{planet_name}(kg): "
+    mass = gets.chomp.to_f
+    print "\nInteresting fact about #{planet_name}: "
+    fun_fact = gets.chomp.to_s
+    print "\nPlease rate the planet's twang on a scale of 0-5.0 #{planet_name}: "
+    twang_level = gets.chomp.to_f
+    print "\nPlease rate the planet's likelihood to be beloved in New England, on a scale of 0-100 #{planet_name}: "
+    likelihood_to_be_loved_by_yanks = gets.chomp.to_s
+
+    new_planet = Planet.new(planet_name, color, distance, mass, fun_fact, twang_level, likelihood_to_be_loved_by_yanks)
+
+    add_planet(new_planet)
+    print "\nGenerating new data"
+  end
+
+  # create list of planets
 
 end
