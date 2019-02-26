4b91a302b45624abbd563c2ca96948b5f865d81a
Bitaman <b.amani.s@gmail.com>
2019-02-25 16:13:02 -0800

    Finished wave 2

--- main.rb
+++ main.rb
@@ -1,11 +1,24 @@
 require_relative "planet"
+require_relative "solar_system"
 
 def main
+  solar_system = SolarSystem.new("Sun")
+
   jupiter = Planet.new("Jupiter", "multicolor", "1.89e27", "7.78e8", "Has more than 75 moons!")
   mars = Planet.new("Mars", "red", "6.41e23", "2.27e8", "Had water in the Ancient Past!")
-  puts jupiter.color
-  puts mars.fun_fact
-  puts mars.summary
+  earth = Planet.new("Earth", "blue-green", "5.972e24", "1.496e8", "Only planet known to support life")
+  solar_system.add_planets(earth)
+  solar_system.add_planets(jupiter)
+  solar_system.add_planets(mars)
+
+  #list = solar_system.list_planets
+  #   puts jupiter.color
+  #   puts mars.fun_fact
+  #   puts mars.summary
+  found_planet = solar_system.find_planet_by_name("Earth")
+  puts found_planet
+  puts found_planet.summary
+  #puts list
 end
 
 main
---
+++ solar_system.rb
@@ -0,0 +1,25 @@
+class SolarSystem
+  attr_reader :star_name, :planet
+
+  def initialize(star_name)
+    @planets = []
+    @star_name = star_name
+  end
+
+  def add_planets(planet)
+    @planets.push(planet)
+  end
+
+  def find_planet_by_name(name)
+    #raise ArgumentError if @planet.include?(name.capitalize)
+    @planets.find { |planet| planet.name.downcase == name.downcase }
+  end
+
+  def list_planets
+    rotating_planets = @planets.map.with_index { |item, index|
+      "#{index + 1}" + "." + item.name
+    }
+    puts "Planets orbiting Sun are: "
+    return rotating_planets
+  end
+end
