3aaec260031de734ee20b9e1c7b2c9093e311739
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-02-26 12:46:51 -0800

    Added control loop

--- lib/main.rb
+++ lib/main.rb
@@ -2,22 +2,45 @@ require_relative "planet"
 require_relative "solar_system"
 
 def main
-  solar_system = SolarSystem.new("Big Ol Pupper")
+  big_ol_pupper = SolarSystem.new("Big Ol Pupper")
   waffles = Planet.new("Waffles", "\'maple syrup\' brown", 3.555e23, 1.273e12, "It is the only known " \
   "planet to be smothered in fried chicken")
-  cuddles = Planet.new("Cuddles", "soft velvet grey", 9.973e30, 0.000002, "This planet loves the sun so much " \
-  "that it can\'t get close enough to it!")
-  solar_system.add_planet(waffles)
-  solar_system.add_planet(cuddles)
-  list = solar_system.list_planets
-  puts list
+  cuddles = Planet.new("Cuddles", "soft velvet grey", 9.973e30, 0.000002, "This planet loves the Big Ol " \
+  "Pupper so much that it can\'t get close enough to it")
+  whiskey = Planet.new("Whiskey", "rich amber", 4.342e27, 3.473e13, "This planet has been aged for" \
+  "billions of years in a giant cosmic cedar barrel")
+  bear = Planet.new("Bear", "jet black", 5.342e20, 5.342e14, "Contains more hugs per capita than any other" \
+  "known planet")
+  pancake = Planet.new("Pancake", "light brown", 7.888e24, 3.342e5, "There is no planet fluffier or " \
+  "flatter than Pancake")
 
-  found_planet = solar_system.find_planet_by_name("Cuddles")
-  # found_planet is an instance of class Planet
-  puts found_planet
-  # => #<Planet:0x00007fe7c2868ee8>
-  puts found_planet.summary
-  # => Earth is a blue-green planet ...
+  big_ol_pupper.add_planet(waffles)
+  big_ol_pupper.add_planet(cuddles)
+  big_ol_pupper.add_planet(whiskey)
+  big_ol_pupper.add_planet(bear)
+  big_ol_pupper.add_planet(pancake)
+
+  answer = "list planets"
+  while answer == "list planets"
+    print "What would you like to do next? (list planets, or exit): "
+    selection = gets.chomp.downcase
+
+    if selection == "list planets"
+      puts big_ol_pupper.list_planets
+    elsif selection == "exit"
+      break
+    end
+  end
+
+  # list = solar_system.list_planets
+  # puts list
+
+  # found_planet = solar_system.find_planet_by_name("Cuddles")
+  # # found_planet is an instance of class Planet
+  # puts found_planet
+  # # => #<Planet:0x00007fe7c2868ee8>
+  # puts found_planet.summary
+  # # => Earth is a blue-green planet ...
 
 end
 
