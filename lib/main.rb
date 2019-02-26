157c3277be5a772da0078714321f75b6cf2a777f
Kelly Downes <kedownes@gmail.com>
2019-02-25 16:24:30 -0800

    Create SolarSystem class and add instance and method calls to main

--- lib/main.rb
+++ lib/main.rb
@@ -1,10 +1,17 @@
 require_relative "planet.rb"
+require_relative "solar_system.rb"
 
 def main
+  solar_system = SolarSystem.new("Sol")
   mercury = Planet.new("Mercury", "gray", 0.330, 57909227, "It's hot, but not too hot for ice")
-  puts mercury.summary
+  solar_system.add_planet(mercury)
   venus = Planet.new("Venus", "pale yellow", 4.87, 108209475, "Doesn’t have any moons, and we aren’t sure why.")
-  puts venus.summary
+  solar_system.add_planet(venus)
+  found_planet = solar_system.find_planet_by_name("Venus")
+  puts found_planet
+  puts found_planet.summary
+  list = solar_system.list_planets
+  puts list
 end
 
 main
---
+++ lib/solar_system.rb
@@ -0,0 +1,30 @@
+class SolarSystem
+  attr_reader :star_name, :planets
+
+  def initialize(star_name)
+    @star_name = star_name
+    @planets = []
+  end
+
+  def add_planet(planet)
+    @planets.push(planet)
+  end
+
+  def list_planets
+    planet_list = "Planets orbiting #{@star_name}\n"
+    @planets.each_with_index do |planet, index|
+      planet_list += "#{index + 1}. #{planet.name}\n"
+    end
+    return planet_list
+  end
+
+  def find_planet_by_name(planet)
+    @planets.each do |planet|
+      if planet.name.downcase == planet.name.downcase
+        return planet
+      end
+    end
+    return "No planet named #{planet} is currently in this solar system. " \
+           "Please add planet to the solar system using add_planet."
+  end
+end
