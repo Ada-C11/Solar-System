5d1389aeb47092cb37c05358d633c723bed3ca30
qqdipps <sdipp007@gmail.com>
2019-02-26 13:27:33 -0800

    added missing methods, put in module, still working out main structure

--- lib/main.rb
+++ lib/main.rb
@@ -1,55 +1,69 @@
 require_relative "planet.rb"
 require_relative "solar_system.rb"
 
-def main
-  display(load_solar_sytem_program)
-  continue = true
-  while continue
-    display(options)
-    continue = do_option(get_option)
+module CLI_SOLAR_SYSTEM
+  attr_reader :solar_system
+
+  def main
+    display(load_solar_sytem_program)
+    continue = true
+    while continue
+      display(options)
+      continue = do_option(get_option)
+    end
+    display(good_bye)
   end
-  display(good_bye)
-end
 
-def display(string)
-  puts string
-  return true
-end
+  def display(string)
+    puts string
+    return true
+  end
 
-def load_solar_sytem_program
-  err_system = SolarSystem.new("Err")
-  create_system(err_system)
-  return "Welcome to the Solar System Program. \n ou are in the Solar System of #{err_system.name}."
-end
+  def load_solar_sytem_program
+    solar_system = SolarSystem.new("Err")
+    create_system(solar_system)
+    return "Welcome to the Solar System Program. \n ou are in the Solar System of #{err_system.name}."
+  end
 
-def create_system(solar_system)
-  solar_system.add_planet(Planet.new("Willerr", "blue", 5.972e24, 1.496e28, "raining rainbows"))
-  solar_system.add_planet(Planet.new("Saverr", "amber", 9.34e54, 2.345e23, "saving time by reversing spin"))
-  solar_system.add_planet(Planet.new("Katerr", "green", 2.342e33, 4.234e43, "being made of dolphins"))
-  solar_system.add_planet(Planet.new("Singherr", "onyx", 4.23e23, 46.2e23, "emmitting song-like sounds"))
-end
+  def create_system(solar_system)
+    solar_system.add_planet(Planet.new("Willerr", "blue", 5.972e24, 1.496e28, "raining rainbows"))
+    solar_system.add_planet(Planet.new("Saverr", "amber", 9.34e54, 2.345e23, "saving time by reversing spin"))
+    solar_system.add_planet(Planet.new("Katerr", "green", 2.342e33, 4.234e43, "being made of dolphins"))
+    solar_system.add_planet(Planet.new("Singherr", "onyx", 4.23e23, 46.2e23, "emmitting song-like sounds"))
+  end
 
-def options
-  return "\nEnter LIST to display all the planets in system."
-  +"\nEnter ADD to add planets"
-  +"\mEnter CALC to calculate the distance between two planets"
-  +"\nEnter INFO to display information about a planet"
-  +"\nEnter Q to quit program"
-end
+  def options
+    return "\nEnter LIST to display all the planets in system."
+    +"\nEnter ADD to add planets"
+    +"\mEnter CALC to calculate the distance between two planets"
+    +"\nEnter INFO to display information about a planet"
+    +"\nEnter Q to quit program"
+  end
 
-def get_option
-  print "Please Enter Selection: "
-  user_input = gets.chomp.upcase
-  return user_input if ["LIST", "ADD", "CALC", "INFO", "Q"].include?(user_input)
-  puts "Hmm... I don't understand."
-  get_option
-end
+  def get_option
+    print "Please Enter Selection: "
+    user_input = gets.chomp.upcase
+    return user_input if ["LIST", "ADD", "CALC", "INFO", "Q"].include?(user_input)
+    puts "Hmm... I don't understand."
+    get_option
+  end
 
-def do_option(option_key)
-  case option_key
-  when "LIST"
-  when "ADD"
-  when "CALC"
-  when "Q"
+  def do_option(option_key)
+    case option_key
+    when "LIST"
+      display(solar_system.list_planets)
+    when "ADD"
+      user_add_planet
+    when "CALC"
+      user_calc_distance
+    when "Q"
+      return false
+    end
+  end
+
+  def user_add_planet
+  end
+
+  def user_calc_distance
   end
 end
