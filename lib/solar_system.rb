d523f390d8f1110e25db3a6a3965f286bb9f7326
qqdipps <sdipp007@gmail.com>
2019-02-25 16:47:09 -0800

    added tests and methods for list_planets, add_planets in solar_system.

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -2,12 +2,20 @@ class SolarSystem
   attr_reader :name, :planets
 
   def initialize(name)
-    @name = name
+    @name = name.capitalize
     @planets = Array.new
   end
 
   def add_planet(instance_of_planet)
     @planets.push(instance_of_planet)
     return true
+  end
+
+  def list_planets
+    string_planets = "Planets orbiting #{self.name}"
+    @planets.each_with_index do |planet, i|
+      string_planets += "\n#{i + 1}. #{planet.name}"
+    end
+    return string_planets
   end
 end
--- specs/solar_system_spec.rb
+++ specs/solar_system_spec.rb
@@ -13,14 +13,35 @@ describe "SolarSystem" do
   end
 
   it "has a name" do
-    expect(sol_system.name).must_equal "sol"
+    expect(sol_system.name).must_equal "Sol"
   end
 
   describe "SolarSystem#add_planet" do
-    it "has a plant instance added to planets" do
-      earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "Only planet known to support life")
-      sol_system.add_planet(earth)
+    let(:earth) {
+      Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "Only planet known to support life")
+    }
+    let(:mars) {
+      Planet.new("mars", "red", 5.9234, 1.2348, "Only planet thats red and dusty")
+    }
+    before do
+      planets_test = [earth, mars]
+      planets_test.each do |planet|
+        sol_system.add_planet(planet)
+      end
+    end
+
+    it "has a planet instance added to planets" do
       expect(sol_system.planets).must_include earth
+    end
+
+    describe "SolarSystem#list_planets" do
+      it "returns a string" do
+        expect(sol_system.list_planets).must_be_instance_of String
+      end
+
+      it "returns formatted list of sol_system" do
+        expect(sol_system.list_planets).must_equal "Planets orbiting Sol\n1. Earth\n2. Mars"
+      end
     end
   end
 end
