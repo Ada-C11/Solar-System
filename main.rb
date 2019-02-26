cd4521f331343a3824e91daf2e57ee0f2ad96498
Kate Sandler <kate@katekatekate.net>
2019-02-25 18:03:40 -0800

    adds to method (solar system)

--- main.rb
+++ main.rb
@@ -1,16 +1,35 @@
 require_relative 'planet'
+require_relative 'solar_system'
+require 'colorize'
 
-def greeting
+def main
+p1 = Planet.new("Plutern", "basil green", 494, 1209, "Plutern was the first planet ever recorded by the process of mechanical scopeometry")
+p2 = Planet.new("Eantune", "yolky", 332, 785, "Eantune represents simulacrity in the astrological divination system of the people of Plutern")
+solar_system = SolarSystem.new('Sun 4')
+solar_system.add_planet(p1)
+solar_system.add_planet(p2)
+
+list = solar_system.list_planets
+
+found_planet = solar_system.find_planet_by_name("eantune")
+
+puts list
+puts found_planet
+puts found_planet.summary
 end
 
-def manifest_spheres
-end
+main
+# def greeting
+# end
 
-def user_chooser
-end
+# def manifest_spheres
+# end
 
-def planet_picker
-end
+# def user_chooser
+# end
+
+# def planet_picker
+# end
 
 def adventure
 end
