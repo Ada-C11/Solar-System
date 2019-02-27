4a7ca60eabbfffcd75d714e30dbce64efdfb6ca3
Cloudy Lopez <cloudylopez@gmail.com>
2019-02-26 18:49:10 -0800

    ugggh done

--- main.rb
+++ main.rb
@@ -1,34 +1,66 @@
 require_relative "planet.rb"
-require_relative "solar_system"
+require_relative "solar_system.rb"
+
+def main_menu
+  puts "Welcome to make yo own planet!"
+  puts "What would you like to do next? You can choose from: \n 1. List Planets \n 2. Planet Details \n 3. Add Planet \n 4. Exit"
+  main
+end
 
 def main
-    earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "Only planet known to support life")
+  earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "Only planet known to support life")
   cloudster = Planet.new("Cloudster", "pink", 5, 23, "Hello Kitty Universe planet")
   puffy = Planet.new("Puffy", "periwinkle", 124234, 325623, "Hello Kitte bioverse")
-  glitterish = Planet.new("Glitterish", "Surruliean", 35435472, 23513541, "Hello Kitty world with lots of water and grass")
- 
-  # Solar System 1
-  solar_system = SolarSystem.new("Ethereal")
-  solar_system.add_planet(earth)
-  solar_system.add_planet(cloudster)
-  list = solar_system.list_planets
+  glitterish = Planet.new("Glitterish", "surruliean", 35435472, 23513541, "Hello Kitty world with lots of water and grass")
   
-  # Solar System 2
-  two_solar_system_ = SolarSystem.new("Isometric")
-  two_solar_system.add_planet(puffy)
-  two_solar_system.add_planet(glitterish)
-  two_list = two_solar_system.list_planets
+     # Solar System 1
+    solar_system = SolarSystem.new("Ethereal")
+    solar_system.add_planets(earth)
+    solar_system.add_planets(cloudster)
+    list = solar_system.list_planets
 
-# Control Loop
-puts "What would you like to do next? You can choose from: $\ List planets $\ Exit"
-answer = gets.chomp 
+  # solar_system = SolarSystem.new("Ethereal")
+  # solar_system.add_planet(earth)
+  # solar_system.add_planet(cloudster)
+  # solar_system.add_planet(puffy)
+  # solar_system.add_planet(glitterish)
 
+  user_input = "y"
+  while user_input == "y"
+      puts $\
+      puts "Make a selection"
+      choice = gets.chomp.to_i
 
+    list = solar_system.list_planets
 
-
-  found_planet = solar_system.find_planet_by_name("Earth")
-  puts found_planet
-  puts found_planet.summary
+    if choice == 1 
+      print list
+    elsif choice == 2 
+      puts "Lets find out more about the planets"
+      print list 
+      planet_info = gets.chomp
+      found_planet = solar_system.find_planet_by_name(planet_info)
+    
+      if found_planet
+        puts "You have chosen #{found_planet.name}."
+        puts found_planet.summary
+      end
+    
+    elsif choice == 3 
+      solar_system.create_planet
+    elsif choice == 4 
+      answer = "y"
+      while answer == "y"
+        puts "Do you want to play again? Yes or No?"
+        answer = gets.chomp 
+        if answer == "yes" 
+          main_menu
+        else 
+          break
+        end
+      end
+      exit
+    end
+  end 
 end
-main
-
+main_menu
--- solar_system.rb
+++ solar_system.rb
@@ -1,7 +1,7 @@
 class SolarSystem 
-    attr_reader :star_name :planets
+    attr_reader :star_name, :planets
 
-    def intialize(star_name)
+    def initialize(star_name)
         @star_name = star_name
         @planets = []
     end
@@ -22,5 +22,23 @@ class SolarSystem
             planet.name == planet_name.capitalize
         end
     end
-end
+    
+    def create_planet
+        puts "To make a new planet, enter planet details"
+        puts "enter the name" 
+        new_name = gets.chomp
+        puts "what color is the planet?"
+        new_color_planet = gets.chomp
+        puts "whats the mass in kg"
+        new_mass_kg = gets.chomp.to_f
+        puts "what is the distance from the sun"
+        new_distance_from_sun = gets.chomp.to_i
+        puts "tell me your planets fun fact"
+        new_fun_fact = gets.chomp
 
+    user_planet = Planet.new(new_name, new_color_planet,new_mass_kg, new_distance_from_sun, new_fun_fact)
+    add_planets(user_planet)
+    end 
+
+    
+end
