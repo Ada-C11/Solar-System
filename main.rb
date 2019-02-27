9518f63d1a8c3d20fa6f7fc3c30adf29ed40cecd
Shamira <marshallscm@gmail.com>
2019-02-26 18:06:01 -0800

    solar system project finished

---
+++ main.rb
@@ -0,0 +1,70 @@
+require_relative "solar_system"
+require_relative "planet"
+
+def main
+  sol_system = SolarSystem.new("Sol")
+  mercury = Planet.new("Mercury", "red", 78939, 2000, "")
+  sol_system.add_planet(mercury)
+
+  venus = Planet.new("Venus", "red", 78939, 2000, "")
+  sol_system.add_planet(venus)
+
+  earth = Planet.new("Earth", "green", 100.25, 5000, "I live here!")
+  sol_system.add_planet(earth)
+
+  mars = Planet.new("Mars", "red", 78939, 2000, "John Jones is from there")
+  sol_system.add_planet(mars)
+
+  jupiter = Planet.new("Jupiter", "red", 78939, 2000, "")
+  sol_system.add_planet(jupiter)
+
+  saturn = Planet.new("Saturn", "purple", 78939, 2000, "")
+  sol_system.add_planet(saturn)
+
+  uranus = Planet.new("Uranus", "red", 78939, 2000, "")
+  sol_system.add_planet(uranus)
+
+  neptune = Planet.new("Neptune", "red", 78939, 2000, "")
+  sol_system.add_planet(neptune)
+
+  list = sol_system.list_planets
+
+  puts "one"
+  puts sol_system.list_planets_one
+  #will return a summary of the planet you put in the argument
+  sol_system.find_planet_by_name_two("maRs").summary
+
+  puts "Would you like a list of planets, planet details, or to exit?"
+  puts "Enter List to continue or exit to end the program"
+
+  # gets user input
+
+  loop do
+    list = sol_system.list_planets
+    input = gets.chomp.downcase
+    break if input == "exit"
+
+    case input
+    when "list"
+      puts list
+    when "details"
+      puts "What planet do you want details for?"
+      details = gets.chomp.downcase
+      puts sol_system.find_planet_by_name_two(details).summary
+    when "add"
+      sol_system.new_planet
+      puts sol_system.list_planets
+    end
+    break
+  end
+
+  puts "Goodbye!"
+
+  # def planet_details(details)
+  #   planet_summary = @planets.each do |planet|
+  #     planet
+  #   end
+  # end
+end
+
+main
---
+++ solar_system.rb
@@ -0,0 +1,68 @@
+
+
+class SolarSystem
+  attr_reader :star_name, :planets
+
+  def initialize(star_name)
+    @star_name = star_name
+    @planets = []
+  end
+
+  # Create a method add_planet,
+  #which will take an instance of Planet as a parameter and add it to the list of planets.
+  def add_planet(planet) #Planet.name
+    #iterate through each Planet.name and put it in the @planets array.
+    @planets << planet
+  end
+
+  def list_planets
+    result = "Planets orbitting:\n"
+    planet_names = @planets.map do |planet|
+      planet.name
+    end
+    planet_names.each_with_index do |name, index|
+      result += "#{index + 1}. #{name}\n"
+    end
+    return result
+    # @planets.name specify that you're searching the name
+  end
+
+  def list_planets_one
+    result = "Planets orbitting:\n"
+    @planets.each_with_index do |planet, index|
+      result += "#{index + 1}. #{planet.name}\n"
+    end
+    return result
+  end
+
+  def find_planet_by_name(name)
+    lowercase_name_to_planet_hash = {}
+    @planets.each do |planet|
+      lowercase_name_to_planet_hash[planet.name.downcase] = planet
+    end
+    return lowercase_name_to_planet_hash[name.downcase]
+  end
+
+  def find_planet_by_name_two(name)
+    return @planets.find do |planet|
+             planet.name.downcase == name.downcase
+           end
+  end
+
+  def new_planet
+    puts "Add planet name:"
+    name = gets.chomp.downcase
+    puts "Add color:"
+    color = gets.chomp
+    puts "Add mass in kg:"
+    mass_kg = gets.chomp.to_i
+    puts "Distance from the sun?"
+    distance_from_sun_km = gets.chomp.to_i
+    puts "What is a fun fact about your planet?"
+    fun_fact = gets.chomp
+    user_input_planet = (Planet.new(name, color, mass_kg, distance_from_sun_km, fun_fact))
+    # planet(user_input_planet)
+    # puts sol_system.new_planet.summary
+    add_planet(user_input_planet)
+  end
+end
