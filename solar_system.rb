37d3d22b80faf6604800f003779e8a9ca8f5f0e4
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-02-26 21:36:59 -0800

    Added a new method to add a new planet

--- solar_system.rb
+++ solar_system.rb
@@ -11,7 +11,11 @@ class SolarSystem
 
   # Take an instance of Planet as a parameter and add it to the list of planets.
   def add_planet(planet)
-    @planets << planet.name
+    planets = @planets.map { |i| i.name.downcase }
+    if planets.include?(planet.name.downcase)
+      raise ArgumentError.new("Planet already exists. A solar system cannot have duplicate planets")
+    end
+    @planets.push(planet)
   end
 
   puts 
@@ -27,19 +31,36 @@ class SolarSystem
     def list_planets
       list = ""
       for i in (0..@planets.length - 1)
-        list += "\n#{i + 1}. #{@planets[i]}"
+        list += "\n#{i + 1}. #{@planets[i].name}"
       end
       return list
     end
 
-    def find_planet_by_name(planet_name)
-      # found_planet = ""
-      for i in (0..@planets.length - 1)
-        if @planets[i] == planet_name
-          # found_planet = planet_name
-          return @planets[i]
-        end
+    def find_planet_by_name(name)
+      @planets.each do |planet|
+          if planet.name.downcase == name.downcase
+              return planet
+          end
       end
-      # return found_planet
+      return nil
+    end
+
+    def discovered_planet
+
+      puts "What is the name of your planet?"
+      name = gets.chomp.capitalize
+      puts "What is the color of your planet?"
+      color = gets.chomp.downcase
+      puts "How much does your planet weigh in kg?"
+      mass_kg = gets.chomp.to_f
+      puts "How far is your planet from the sun in km?"
+      distance_from_sun_km = gets.chomp.to_f
+      puts "What is a fun fact about your planet?"
+      fun_fact = gets.chomp.downcase
+
+      new_planet = Planet.new(name, color, mass_kg, distance_from_sun_km, fun_fact)
+      add_planet(new_planet)
+
+      # puts new_planet.name
     end
 end
