ce6a4d9e100a87f51f32959bb5547f8766a9c0f2
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-02-26 18:15:45 -0800

    Wave 3 completed

--- main.rb
+++ main.rb
@@ -12,57 +12,44 @@ def main
   solar_system.add_planet(earth)
   solar_system.add_planet(astra)
 
-  list = solar_system.list_planets
+  # caching results => list = solar_system.list_planets
 
   # Solar System # 2
   solar_system2 = SolarSystem.new("Dol")
   solar_system2.add_planet(ego)
   solar_system2.add_planet(namek)
 
-  list2 = solar_system2.list_planets
-
-  #
-  #
-  #
-
-  puts "What would you like to do now? (list planets/ planet details/ add planet/ exit) >"
-  response = gets.chomp.downcase
-
-  case response
-  when "list planets"
-    def what_to_do
-      puts "What should we do next? (list planets/ planet details/ add planet/ exit) >"
-      response = gets.chomp.downcase
-    end
-
-    puts "Which universe? (1 or 2)"
-    response2 = gets.chomp
-    if response2 == "1"
-      puts "#{list}"
-      what_to_do
-    elsif response2 == "2"
-      puts "#{list2}"
-      what_to_do
-    else
-      puts "This is not an option."
-      what_to_do
-    end
-  when "planet details"
-    puts "What is the name of the planet?"
-    planet_detail = gets.chomp.capitalize
-    found_planet = solar_system.find_planet_by_name(planet_detail)
-    puts found_planet.summary
-    what_to_do
-    # when "add planet"
-    #   puts
-  when "exit"
-    exit
+  def what_to_do
+    puts "What should we do next? (list planets/ planet details/ add planet/ exit):"
+    return gets.chomp.downcase
   end
 
-  #
-  #
-  #
-  #
+  continue = true
+
+  while (continue)
+    response = what_to_do
+    case response
+    when "list planets"
+      puts "Which universe? (1 or 2)"
+      response = gets.chomp
+      if response == "1"
+        puts "#{solar_system.list_planets}"
+      elsif response == "2"
+        puts "#{solar_system2.list_planets}"
+      else
+        puts "This is not an option."
+      end
+    when "planet details"
+      puts "What is the name of the planet?"
+      planet_detail = gets.chomp.capitalize
+      found_planet = solar_system.find_planet_by_name(planet_detail)
+      puts found_planet.summary
+    when "add planet"
+      solar_system.new_planet_information
+    when "exit"
+      continue = false
+    end
+  end
 end
 
 main
--- solar_system.rb
+++ solar_system.rb
@@ -23,6 +23,22 @@ class SolarSystem
            end
   end
 
+  def new_planet_information
+    puts "What is the name of the planet?"
+    new_name = gets.chomp
+    puts "What color is #{new_name}?"
+    new_color = gets.chomp
+    puts "What is the mass in kg of #{new_name}?"
+    new_mass = gets.chomp
+    puts "How far is #{new_name} from the sun in km?"
+    new_distance = gets.chomp
+    puts "What is a fun fact about #{new_name}?"
+    new_fun_fact = gets.chomp
+
+    new_planet = Planet.new(new_name, new_color, new_mass, new_distance, new_fun_fact)
+    add_planet(new_planet)
+  end
+
   # Wave 2 optional step 6
   # def distance_between(planet1, planet2)
   #   value = @planets.name(planet1). - planet2
