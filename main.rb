c4b3c8117a7cad6686ecd531e7aae319c12331ab
Karla Guadron <ksguadron@gmail.com>
2019-02-26 18:03:10 -0800

    Wave 3: restructured main.rb, added contorl loop with list planet, planet details, and add planet

--- main.rb
+++ main.rb
@@ -1,4 +1,3 @@
-
 require_relative "planet"
 require_relative "solar_system"
 require "terminal-table"
@@ -18,18 +17,57 @@ def main
   jupiter = Planet.new("Jupiter", "yellow", 1898e24, 778.6e6, "Jupier has 67 moons")
   solar_system.add_planet(jupiter)
 
-  puts earth.summary
-  puts mars.summary
-  puts venus.summary
-  puts jupiter.summary
+  loop do
+    puts "Welcome to the Planet Info Machine"
+    puts "What would you like to do?" 
+    puts "Enter the number of your choice:
+      1. List planets
+      2. Get planet details
+      3. Add a new planet
+      4. Exit"
+      choice = gets.chomp.to_i
 
-  solar_system.list_planets
-
-  found_planet = solar_system.find_planet_by_name('Earth')
-  puts found_planet
-  found_planet.each { |each_planet| puts each_planet.summary }
-  # calling .summary on an array but it need to be on individual element/string of array
-
+    case choice
+      when 1
+        puts solar_system.list_planets
+      when 2
+        puts "What planet would you like details on?"
+        user_planet = gets.chomp.downcase
+        puts solar_system.find_planet_by_name(user_planet)
+      when 3
+        solar_system.make_new_planet
+      when 4
+        "BYE"
+        exit
+    end
+  end  
 end
 
 main
+
+  # until choice == "exit" do
+  #   if choice == "list planets"
+  #     puts solar_system.list_planets
+  #   elsif choice == "planet details"
+  #     puts planet_details
+  #   end
+
+  #   puts "What would you like to do next? (list planets, planet details, exit)"
+  #   choice = gets.chomp.downcase
+  # end
+
+  # puts earth.summary
+  # puts mars.summary
+  # puts venus.summary
+  # puts jupiter.summary
+
+  # solar_system.list_planets
+
+  # found_planet = solar_system.find_planet_by_name('Venus')
+  # puts found_planet
+
+  # def planet_details
+  #   puts "What planet would you like details on?"
+  #   user_planet = gets.chomp.downcase
+  #   return solar_system.find_planet_by_name(user_planet)
+  # end
--- solar_system.rb
+++ solar_system.rb
@@ -1,5 +1,7 @@
 class SolarSystem
-  attr_reader :star_name, :planets
+  attr_reader :star_name
+
+  attr_accessor :planet
 
   def initialize(star_name)
     @star_name = star_name
@@ -11,16 +13,31 @@ class SolarSystem
   end
 
   def list_planets
-    puts "Planets orbiting #{@star_name}:"
-    @planet.each_with_index do |planet, index| 
-      puts "#{index+1}. #{planet.name}" 
-    end 
+    planet_list = "Planets orbiting #{@star_name}:\n"
+    @planet.each_with_index do |planet, index|
+      planet_list << "#{index + 1}. #{planet.name}\n"
+    end
+    return planet_list
   end
 
   def find_planet_by_name(planet)
     selected_planet = @planet.select { |other_planet| other_planet.name.downcase == planet.downcase }
-    # selected_planet.each { |each_planet| each_planet.summary }
-    
+    selected_planet.each { |each_planet| puts each_planet.summary }
+  end
+
+  def make_new_planet
+    puts "What is the name of your planet?"
+    user_planet = gets.chomp.capitalize
+    puts "What color is the planet?"
+    user_color = gets.chomp
+    puts "What is the planet's mass (in kg)?"
+    user_mass = gets.chomp
+    puts "What is the planet's distance from the sun (in km)"
+    user_distance = gets.chomp
+    puts "What is a fun fact about this planet?"
+    user_fact = gets.chomp
+    new_planet = Planet.new(user_planet, user_color, user_mass, user_distance, user_fact)
+    add_planet(new_planet)
+    puts "Your planet has been added"
   end
 end
-
