318f8f523cdfdf5ac33dcc704896c4fce21280c5
Amy Wyatt <amy.wyatt12@outlook.com>
2019-02-27 11:53:17 -0800

    Added add planet functionality

--- main.rb
+++ main.rb
@@ -2,37 +2,76 @@ require_relative "planet"
 require_relative "solar_system"
 
 def main
+  # creating solarsystem and planets
   sun = SolarSystem.new("Sun")
   venus = Planet.new("Venus", "teal", 23534623565, 43563546345, "they claim women are from here, but they are wrong")
-  earth = Planet.new("Earth", "blue-green", 5.972e24, 1.496e8, "only planet known to support life")
-  mars = Planet.new("Mars", "red", 4.972e24, 0.496e8, "rich people want to live here in the future")
+  earth = Planet.new("Earth", "blue-green", 56354677465487, 14624573674526, "only planet known to support life")
+  mars = Planet.new("Mars", "red", 4674567655, 2457365426, "rich people want to live here in the future")
 
+  # adding planets to solarsystem
   sun.add_planet(venus)
   sun.add_planet(earth)
   sun.add_planet(mars)
 
+  # displays welcome and options menu to user
   puts "\nWelcome to the solar system simulator!"
   puts "\nWhat would you like to do?"
   puts "List planets"
   puts "Planet details"
+  puts "Add planet"
   puts "Exit\n\n"
 
-  command = gets.chomp.downcase.capitalize!
+  # takes user input
+  command = gets.chomp.capitalize!
 
+  # determines program response based on user input
   while command != nil
-    if command == "List planets"
+    if command == "List planets" # lists planets in solarsystem
       list = sun.list_planets
       puts "\n#{list}"
-    elsif command == "Planet details"
+    elsif command == "Planet details" # displays details about specific planet
       puts "Which planet would you like to learn more about?"
-      selected_planet = gets.chomp.downcase.capitalize!
-
-      if sun.planets.include?(selected_planet)
-        puts selected_planet.summary
+      selected_planet = gets.chomp.capitalize!
+      found_planet = sun.find_planet_by_name(selected_planet)
+      if found_planet != nil
+        puts "\n#{found_planet.summary}"
       else
         puts "\nSorry, we don't have information on that planet at this time."
       end
-    elsif command == "Exit"
+    elsif command == "Add planet" # adds new planet to solar system
+      new_planet_name = ""
+      while new_planet_name == ""
+        puts "Please name the planet you would like to add."
+        new_planet_name = gets.chomp
+      end
+
+      new_planet_color = ""
+      while new_planet_color == ""
+        puts "Please enter the planet color."
+        new_planet_color = gets.chomp
+      end
+
+      new_planet_mass = ""
+      while new_planet_mass == ""
+        puts "Please enter the mass of the planet in kg."
+        new_planet_mass = gets.chomp
+      end
+
+      new_planet_distance_from_sun = ""
+      while new_planet_distance_from_sun == ""
+        puts "Please enter this planet's distance from the sun in km."
+        new_planet_distance_from_sun = gets.chomp
+      end
+
+      new_planet_fun_fact = ""
+      while new_planet_fun_fact == ""
+        puts "Please enter a fun fact about this planet."
+        new_planet_fun_fact = gets.chomp
+      end
+
+      new_planet_info = Planet.new(new_planet_name, new_planet_color, new_planet_mass, new_planet_distance_from_sun, new_planet_fun_fact)
+      sun.add_planet(new_planet_info)
+    elsif command == "Exit" # exits program
       puts "\nAre you sure you want to exit? Please type Y to exit."
       answer = gets.chomp.upcase!
       if answer == "Y"
@@ -42,13 +81,17 @@ def main
       puts "\nThat is not a valid command, please try typing a option from the list."
     end
 
+    # lists user options
     puts "\nWhat would you like to do next?"
     puts "List planets"
     puts "Planet details"
+    puts "Add planet"
     puts "Exit\n\n"
 
+    # gets user input
     command = gets.chomp.downcase.capitalize!
   end
 end
 
+# runs code
 main
--- solar_system.rb
+++ solar_system.rb
@@ -6,10 +6,12 @@ class SolarSystem
     @planets = []
   end
 
+  # add planet to the solar system
   def add_planet(planet)
     @planets << planet
   end
 
+  # displays planets in solar system
   def list_planets
     planet_list = "Planets orbiting #{@star_name}:"
     @planets.each_with_index do |planet, index|
@@ -18,15 +20,17 @@ class SolarSystem
     return planet_list
   end
 
+  # searches planet list for planet user inputs
   def find_planet_by_name(planet)
+    found_planet = nil
     search_planet = planet.downcase.capitalize!
 
     @planets.each do |planet|
       if search_planet == planet.name
-        @found_planet = planet
+        found_planet = planet
       end
     end
 
-    return @found_planet
+    return found_planet
   end
 end
