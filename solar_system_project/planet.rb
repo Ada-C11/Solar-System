68bd0dc75cf48809f9c0908e20fc074cdd83f6da
Riyo <riyo.perry@gmail.com>
2019-02-26 17:01:08 -0800

    Finished project

---
+++ solar_system_project/planet.rb
@@ -0,0 +1,20 @@
+require "pry"
+
+class Planet
+  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+
+  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_sun_km = distance_from_sun_km
+    @fun_fact = fun_fact
+  end
+
+  def summary
+    return "The name of this planet is #{name}.
+     It is the beautiful shade of #{color}. 
+     It weighs #{mass_kg} kilograms and is #{distance_from_sun_km} kilometers from the sun.
+     A fun fact about #{name} is #{fun_fact}"
+  end
+end
