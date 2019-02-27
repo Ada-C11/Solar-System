780faeb85b2852c31ed19a99f72a624400f196c1
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-02-26 21:36:19 -0800

    Added ArgumentError for mass and distance from sun being < 0 and edited summary output

--- planet.rb
+++ planet.rb
@@ -3,6 +3,15 @@ class Planet
   attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
   # Constructor
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+
+    while mass_kg <= 0
+      raise ArgumentError.new("Planet mass cannot be less than or equal to 0")
+    end
+
+    while distance_from_sun_km <= 0
+      raise ArgumentError.new("Distance from sun cannot be less than or equal to 0. There would be no planet left!")
+    end
+
     @name = name
     @color = color
     @mass_kg = mass_kg
@@ -12,6 +21,6 @@ class Planet
 
   # Summary
   def summary
-    return "#{self.name} is the third planet and is #{self.distance_from_sun_km} kilometers away from the Sun. It's color is #{self.color}. It's mass is #{self.mass_kg} kilograms and it is the #{self.fun_fact}!"
+    return "#{self.name} is #{self.distance_from_sun_km} kilometers away from the Sun. It's color is #{self.color}. It's mass is #{self.mass_kg} kilograms and  #{self.fun_fact}!"
   end
 end
