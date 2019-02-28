09778659b66878d9529345c9d588db7d8027de67
Kate Sandler <kate@katekatekate.net>
2019-02-27 19:07:29 -0800

    Refactored and working driver

--- main.rb
+++ main.rb
@@ -3,57 +3,92 @@ require_relative 'solar_system'
 require 'faker'
 require 'ruby_figlet'
 require 'colorize'
-require 'rubygems'
-require 'highline/import'
+def colors(string)
+  string.chars.map {|char| char.colorize([:red, :cyan, :green, :blue].sample)}.join
+end
+
+def user_add_planet(solar_system)
+  puts "\nAdding a planet is for scientists ONLY. Are you a real scientist? (Y/N)"
+  y_n = gets.chomp.upcase
+    if y_n == "Y"
+      puts "<Computer nods respectfully.>"
+      puts "Please provide some data:"
+      print "Name of planet: "
+      name = gets.chomp
+      print "Surface color: "
+      color = gets.chomp
+      print "Planetary mass in kilograms/10^24?: "
+      mass_kg = gets.chomp.to_f
+      print "Orbital radius in kilometers/10^22: "
+      distance_from_sun = gets.chomp.to_f
+      fun_fact = ""
+      new_planet = Planet.new(name, color, mass_kg, distance_from_sun, fun_fact)
+      solar_system.add_planet(new_planet)
+      puts "\nThank you for the science.\n"
+    else
+      puts "Please try again with more scientific integrity."
+    end
+end
+
+def display_data(solar_system)
+  print "What is the name of the planet you wish to learn about? "
+  query = gets.chomp
+  found_planet = solar_system.find_planet_by_name(query)
+  return found_planet
+end
 
 def main
-  @solar_system = SolarSystem.new(Faker::Space.star)
+  solar_system = SolarSystem.new(Faker::Space.star)
 
-  autofactoid = [
-    "On #{@name}, an atmospheric event called a #{Faker::Games::Pokemon.move} is a rare but spectacular sensory experience, causing a thick, soupy #{Faker::Color.color_name} cloud of #{Faker::Coffee.notes}-scented mist to roll over the surface of the planet",
-    "#{@name} was first spotted by an unnamed #{Faker::Military.marines_rank} working for the #{Faker::Space.agency} who originally named it  #{Faker::Movies::Hobbit.character} in honor of the office dog. The name was only changed to #{@name} in #{rand(1900...1999)}", "#{Faker::GreekPhilosophers.name} famously postulated that #{@planet} was merely a rumor"
-    ]
-
-  6.times do
-  new_planet = Planet.new(Faker::Books::Dune.unique.planet, Faker::Color.color_name, rand(100...10000), rand(100...10000), autofactoid.sample)
-  @solar_system.add_planet(new_planet)
-end
-
-def control_loop
-  begin
-    puts "\n\n"
-    loop do
-      choose do |menu|
-        menu.prompt = "\nPlease select your option: "
-        menu.choice("List Planets") { @solar_system.list_planets }
-        menu.choice("Planet Summary") { @solar_system.elucidate }
-        menu.choice("Add a New Planet") { @solar_system.user_add_planet(@solar_system) }
-        menu.choice("Quit") { exit }
-      end
-    end
+  8.times do |x|
+  new_planet = []
+  new_planet[x] = Planet.new(Faker::Books::Dune.unique.planet, Faker::Color.color_name, rand(100...10000), rand(100...10000), "")
+  solar_system.add_planet(new_planet[x])
+  x += 1
   end
-end
 
-# Start the control loop.
+  loooop = true
+  while loooop == true do
+    puts
+    puts colors("---------------------------------------------------------")
+    puts
+    puts "What would you like to do next?\n"
+    puts "1. List Planets".colorize(:green)
+    puts "2. See Planet Details".colorize(:blue)
+    puts "3. Add Planet".colorize(:magenta)
+    puts "4. Quit".colorize(:red)
+    puts
+    print "Enter the number of your choice: "
+    control = gets.chomp.to_i
+      case control
+      when 1
+        puts "#{solar_system.list_planets}\n"
+      when 2
+        the_query = display_data(solar_system)
+        puts colors(the_query.summary)
+      when 3
+        user_add_planet(solar_system)
+      when 4
+        puts "Goodbye!"
+        exit
+      else
+        puts "That is not a valid choice.\n\n".colorize(:red)
+      end
+      puts colors("---------------------------------------------------------")
+      sleep 1
+    end
+end
 puts "Welcome to ...\n\n"
-sleep 2
-puts <<-'EOF'
-       ▀▄   ▄▀        ▄▄▄████▄▄▄        ▄██▄         ▀▄   ▄▀              ▀▄   ▄▀          ▀▄   ▄▀
-EOF
-sleep 1
-puts <<-'EOF'
-       ▄█▀███▀█▄      ███▀▀██▀▀███     ▄█▀██▀█▄      ▄█▀███▀█▄            ▄█▀███▀█▄        ▄█▀███▀█▄
-      █▀███████▀█     ▀▀███▀▀███▀▀     ▀█▀██▀█▀     █▀███████▀█          █▀███████▀█      █▀███████▀█
-EOF
-sleep 1
-puts <<-'EOF'
-      ▀ ▀▄▄ ▄▄▀ ▀      ▀█▄ ▀▀ ▄█▀      ▀▄    ▄▀     ▀ ▀▄   ▄▀ ▀          ▀ ▀▄▄ ▄▄▀          ▀▄    ▄▀    <3
-
-EOF
-
- space_title = RubyFiglet::Figlet.new "
-o u t e r * s p a c e", 'basic'
+sleep 0.5
+puts colors("       ▀▄   ▄▀        ▄▄▄████▄▄▄        ▄██▄         ▀▄   ▄▀              ▀▄   ▄▀          ▀▄   ▄▀")
+sleep 0.5
+puts colors("       ▄█▀███▀█▄      ███▀▀██▀▀███     ▄█▀██▀█▄      ▄█▀███▀█▄            ▄█▀███▀█▄        ▄█▀███▀█▄")
+sleep 0.6
+puts colors("      █▀███████▀█     ▀▀███▀▀███▀▀     ▀█▀██▀█▀     █▀███████▀█          █▀███████▀█      █▀███████▀█")
+sleep 0.7
+puts colors("      ▀ ▀▄▄ ▄▄▀ ▀      ▀█▄ ▀▀ ▄█▀      ▀▄    ▄▀     ▀ ▀▄   ▄▀ ▀          ▀ ▀▄▄ ▄▄▀          ▀▄    ▄▀    <3")
+puts "\n\n"
+space_title = RubyFiglet::Figlet.new "o u t e r * s p a c e", 'basic'
 space_title.show
-control_loop
-end
+puts "\n"
 main
